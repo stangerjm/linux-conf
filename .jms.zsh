80b101244e4e15ef17106ecebbceff9dd8379f89
James Stanger <jastange@amazon.com>
2021-01-12 18:40:57 +0000

    Added Vi mode and improved super Git show

--- .jms.zsh
+++ .jms.zsh
@@ -2,7 +2,7 @@
 alias nv='nvim'
 alias g='git'
 alias gs='git status'
-alias gS='git status && git branch && git stash list'
+alias gS='echo "STATUS:" && git status && echo "\nBRANCH:" && git branch && echo "\nSTASH:" && git stash list'
 alias gr='git pull origin mainline --rebase'
 alias gc='git commit'
 alias ga='git commit --amend --no-edit'
@@ -15,3 +15,6 @@ alias c='clear'
 alias so='source ~/.zshrc'
 alias to='cd -P'
 alias vw='python -m http.server 8000'
+
+# Vi mode in Bash
+set -o vi
